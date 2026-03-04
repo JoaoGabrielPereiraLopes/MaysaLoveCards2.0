f37b0c4114f7c700ae8fd60def5cbaf83602d6aa
Joâo Gabriel Pereira Lopes <gabriel.pitito123@gmail.com>
2026-03-04 13:49:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 async function CardInit(){
     document.getElementById('Carta').style.animation='5000ms abrir'
     setTimeout(function() {
-        document.getElementById('Carta').style.backgroundImage='url("https://www.bing.com/th/id/OIP.DRyy3X-N2MbXJfGW0M_4TAHaJ4?w=160&h=211&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2")'
+        document.getElementById('Carta').style.backgroundImage='url("./assets/WhatsApp Image 2026-03-02 at 23.57.28.jpeg")'
         document.getElementById('Carta').style.height="90%"
         document.getElementById('Carta').style.width="90%"
+        document.getElementById('Carta').style.backgroundPosition="top right"
         document.getElementById('Carta').innerHTML="<H1>TEXTO TESTE PARA VER SE VOCÊ CONSEGUE LER</H1>"
         
     }, 2500);
