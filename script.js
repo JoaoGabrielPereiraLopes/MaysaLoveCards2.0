ff45e6251131c7b57c4c58323ce103d6fa34b6c7
JoaoGabrielPereiraLopes <gabriel.pitito123@gmail.com>
2026-03-04 19:43:17 -0300

    3 novas cartas

--- script.js
+++ script.js
@@ -1,15 +1,165 @@
+var runing=false
+var next=false
+var titulos=["Uma maravilhosa conhecidência","Sempre vou te amar","Uma imensa sauadade","Uma arte vazia", "A mais bela entre todas","Os olhos mais profundos","O mais lindo vestido","Harmonia","Porque te amo?","Apenas você é bela","Meu dia se torna mais leve"]
+var cards=[
+`Naquele dia, naquela cadeira, o destino foi traçado, é você quem eu irei amar.
+Tudo começou com uma simples conversa, e hoje estamos aqui, juntos, felizes e apaixonados.
+E hoje cá estou eu, escrevendo essa carta, para te dizer o quanto eu te amo, e o quanto você é importante para mim,
+e gastanto os próximos dias escrevendo código.
+e bom, vai valer apena cada minimos detalhe, cada minima coisa, cada letra, cada palavra, cada linha de código.
+Vai valer a pena, pois sei que vou tirar o mais sincero sorriso de você.
+E é por isso que eu te amo, e é por isso que eu estou aqui, escrevendo essa carta.
+Você é uma pessoa incrível, e eu sou grato por ter você em minha vida.
+tão incrível que cada dia,hora,minuto,segundo que gasto com coisas feitas você, 
+é um segundo que eu não me arrependo.
+Essa é a primeira das cartas, das 10 cartas, e agora, eu só quero que você saiba, que eu te amo.
+E bom, para ver as outras apenas toque na carta e você vai ir pra próxima.
+Espero que isso te faça feliz, e que te ranque o mais sincero sorriso.`,
+        
+`Quero dizer que todos os dias, eu penso em você, e em como você é incrível.
+e você nunca,nuncam mesmo, vai saber o quanto você é incrível nos meus olhos.
+Se eu podesse, te daria meus olhos, para que você pudesse ver o quanto você é incrível.
+E bom, eu só quero que você saiba que eu te amo, e que eu estou aqui, escrevendo essa carta.
+Você sempre me ajuda e sempre está ao meu lado, e eu sou grato por isso.
+Em sua compania, eu me sinto bem, e eu me sinto feliz, e completo.
+Mesmo num mundo caótico, você me faz sentir bem, e eu sou grato por isso.
+Não existe coisa melhor do que te amoar, e eu só quero que você saiba, que eu te amo.
+E que tudo isso é sincero, vem do mais profundo do meu coração.
+E desta sensação de altissima felicidade, eu só quero que você saiba, que eu te amo.
+Eu não te amo só na facilidade.
+Eu te amo no fundo do posso, quando você não se vê se quer um resquísio de esperança.
+Eu te amo no alto da vida quando tudo parece colorido e você sabe que venceu.
+Eu te amo a todo momento`,
+        
+`Eu sinto sua falta, ah, a dura saudade.
+Que meu coração corre como em uma maratona, e eu só quero te ver.
+Eu só quero te abraçar, e te beijar, e te dizer o quanto eu te amo.
+Eu quero estar contigo, eu quero você, não importa aonde, eu só quero você.
+Mesmo aquele filme sem graça, aquela comida amarga, ou aquele horrível piada.
+Tudo é divertido com você do meu lado.
+Eu sinto falta, do seu sorriso, do seu jeito, do seu olhar, eu sinto falta de você.
+Não importa aonde, eu só quero você.
+Eu só quero estar ao seu lado, e te dizer o quanto eu te amo.
+Cada risada, cada piada, cada momento, contigo é divertido.
+Até aquele piada que nós já ouvimos 1000 vezes.
+Até aqueles mesmos vídeos que nós já vimos 1000 vezes.
+Aquela mesma música que nós já ouvimos 1000 vezes, a qual em cada nota, me lembra de ti.
+Sem você toda música é apenas ruído, todo filme é apenas mais, toda pintura é apenas uma imagem.`,
+
+`Então todas as músicas são sobre amor, todas as poesias sobre amar, todos os filmes sobre paixão.
+Então todas as músicas,quando não te dedicadas, se tornão nada além de sons vazios e ruídos sem sentido.
+Então todas as poesias,quando não te recitasdas se tornam meras letras jogadas ao vento, sem sentido e sem nexo
+Então todos os filmes, sem você, são meras imagens idiotas e sem sentido.
+Então tudo passou a ter sentido só com você, sem ti até a arte se torna vazia, o arco-íris se torna cinza
+e tudo se reduz a nada, tudo sem sem se tornou nada, até o infinito perdeu o seu valor, até o universou ficou invergonhado de estar vazio com tanto espaço.
+peças de teatro se tornam brincadeiras de imaginar se nexo, os livros de Shaksper se tornam mais um conjunto de simbolos.
+Mas com você tudo muda, o preto e branco ganha cor, o caótico se torna calmo, o pior livro se torna best seller, a pior música vira bethoven e tudo volta a ter cor,
+até o vazio se torna grande, e o universo sente vergonha de não ter nada que consiga explicar sua beleza nem você, nem o infinito. nem o tamanho da mais das galáxias.`,
+
+`Oh, não conheço eu palavras tais que descrevem a ti minha princesa, tu é mais bela que Monalisa,
+és mais graciosas do que as flores, mais encantadora do que as estrelas, és mais cheirosa do que todos os perfumes.
+O universo novamente sente vergonha em não ter nada tal a altura da sua beleza, nem mesmo o infinito sequer chega perto.
+Nem mesmo o brilho de um quasar chega perto dos brilhos profundo de seus olhos, nem mesmo a mais bela constelação
+chega aos pés da forma da sua beleza, nem mesmo a mais bela dentre todas as flores são tão hamoniosas quanto seus cabelos
+Nem mesmo a mais escultural das galáxias, chega perto das curvas do seu corpo, nem mesmo os buracos negros são tão profundos quanto seu olhar.
+Nem mesmo a mais quente das estrelas são tão quentes quanto meu coração ao te olhar.
+Nada então pode espressar o quanto fico impressionado a te observar.
+O universo então ficou envergonhado por não conseguir te descrever.`,
+
+`Os seus olhos meu amor, são mais belos que a mais linda paisagem, pois nem mesmo as paisagem são capazes serem amadas como amo seus olhos.
+Nada pode se igualar a calma que sinto ao te olhar, nem mesmo a linda cena de observar o mar ao luar a beira da praia, afinal o luar não me ama.
+Tentei recorrer a programação mas, nem mesmo os maiores códigos conseguem replicar o seu olhos, pois não são nada além de imagens estáticas.
+Nada parece funcionar nem mesmo a matemática, pois o infinito é pouco perto da profundidade dos seu olhar, e o maior dos numeros, ainda que grandes, diferente da calma dos sseus olhos, acabam.
+Como posso eu, amar algo e não saber explicar?
+Oras, não sou eu que não explicar, mas o português que não consegue ter uma palavra na qual eu consiga totalmente me expressar
+Mas cá estou eu te dedicando, uma carta, enquanto seus olhos estão a brilhar, você deve ter perdido as contas de quantas vezes veio a surtar, e ainda estamos na metade.`,
+
+`- Qual seria o vestido mais bonito de todos?
+- Qualquer um, quanto você pagou na nota, o tamanho do vestido, sua cor, ou se ele é aprovado por uma top-model, nada disso importa.
+- Então como posso eu avaliar o mais belo dentre todos, se o vestido não importa?
+- O mais importante é a modelo que a usa, nenhuma além de Maysa Serpa Ferreira da Silva é capaz de estar bonita.
+- Mas porque? Porque ela é a mais bonita entre todas?
+- Porque ela é simplesmente perfeita, em seu corpo tem curvas que deixam Michelangelo peplexo, em seu olhar tem tal cor que deixa Van Gogh curioso.
+- Mas como poder uma simples pessoa ser o mais bonito em um vestido?
+- Muito simples, nela há a perfeição e em tudo faz por merecer apreciação
+- Acho que reamente o vestido mais bonito de todos, é o que Maysa Serpa Ferreira da Silva usar.
+`,
+
+`Qual a mais hamônica dentre as coisas? Maysa Serpa logo vem a minha cabeça, ela não é só harmonia ela é o equilíbrio perfeito em sua beleza.
+Todas as suas coisas são perfeitamente combinando e se completando. O seu cabelo é o mais lindo de todos, mais mágicos que os da Ranpuzel.
+Seu vestido sempre lhe veste perfeitamente, ela sempre tem curvas maravilhosas, e as cores do vestido é tão vivo que simplesmente me traz cores novamente a minha vida.
+Olhar para ela é como ver a melhor das sortes, Simplesmente te olhar acalma minha mente, me traz de volta a minha paz.
+E eu simplesmente agradeço a Deus por ter me dado você, cada detalhe seu é muito bonito e perfeito, na acho eu como descrever como adoro a sua aparência.
+Tudo em você me lembra que sou capaz, sua beleza é deslumbrante e perfeita a um nível além do infinito.
+Quando te vi me senti sortudo por ter encontrado tal beleza. Quando te olham meus olhos brilham, brilham de tão bonita que você é.`,
+
+`Algumas vezes já me questionei o que me leva a te amar, parece simples, mas esse questionamente sempre me persegue.
+Se te amo pela aparência, porque amo convesar, mesmo sem ver? se te amo pela personalidade, pq te admiro profundamente?
+Se te amo apenas por esses dois, porque te amo nos seus piores dias? No final percebo que não a resposta pois te amo por tudo e por nada.
+Pois tudo me leva a te amar, mas nada é o motivo principal, afinal, poderia dizer que te amo por X ou Y, mas se tudo em você fosse diferente, eu ainda te amaria.
+Portanto concluo que o porque te amo é pq você existe, não existe outro motivo se não sua simples existência por completa, em cada detalhe.
+Em cada segundo descubro um novo pedaço de você que amo. Se estiver insegura, saiba que te amo em cada mínimo detalhe.
+Você não precisa se preocupar com aparência ou sua personalidade, pois enquanto você existir te amarei, mesmo que eu não possua motivos, te amarei pelo simples motivo de você ser você.`,
+
+`Como posso te amar, e não amar a mais ninguém, a partir do momento que te conheci, todos se tornam chatos e entiantes.
+Todos aos quais conheço são meras cópias suas sem graça e sentido, a final, ninguém pode ser mais perfeita que você, e todos os quais são bons, meramente são cópias mau feitas de suas características.
+Nem mesmo os mais importantes e perfeitos atributos, até os mais perfeitos humores são apenas manifestações menos aprimoradas que o seu humar, a final, tu és o alge da perfeição, tudo ao qual tu tens, eu amo ao máximo.
+Portanto, todo o humor é menos engraçado que o seu, toda risada é vazia quando não vinda de você, todo abraço é pouco reconfortante, todo corpo é feio e todo olhar é sem graça.
+Digo isso, pois tudo que amo é você, em todos os detalhes e caracteríscas suas. Eu te amo, até nos seus detalhes que você odeia.`,
+
+`
+Todos os dias são mais leves ao seu lado, todos os segundos se tornam mais toleráveis e todos os momentos se tornam mais felizes, pois você os torna mais leve.
+A sua presença é doce, seu olhar é agradável, sua aparência é encantadora, tudo em você torna meu dia mais bonito, leve e aturável, pelo simples fato de você estar comigo.
+Quero poder te amar, não apenas nos dias fáceis, ou no momento atual, pois até mesmo uma vida toda ao seu lado seria pouco, quero o tempo todo da minha vida, e mais, caso possal.
+Todos os momentos ao seu lado são bons, leves e divertidos, você me tira as mais puras risadas, os mais leves olhares e o mais lindo sorriso em sua face, eu te amo, e quero te amar em todos os detalhes, tempos e sentidos possíveis.
+EU TE AMO EM TODOS OS SENTIDOS, VOCÊ É O AMOR DA MINHA VIDA
+`
+]
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomCard() {
+    const number=Math.floor(Math.random() * titulos.length)
+    return {titulos:titulos[number],text:cards[number]};
+}
 async function CardInit(){
-    document.getElementById('Carta').style.animation='5000ms abrir'
-    setTimeout(function() {
-        document.getElementById('Carta').style.backgroundImage='url("https://www.bing.com/th/id/OIP.DRyy3X-N2MbXJfGW0M_4TAHaJ4?w=160&h=211&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2")'
-        document.getElementById('Carta').style.height="90%"
-        document.getElementById('Carta').style.width="90%"
-        document.getElementById('Carta').innerHTML="<H1>TEXTO TESTE PARA VER SE VOCÊ CONSEGUE LER</H1>"
-        
-    }, 2500);
-    setTimeout(function() {
-        document.getElementById('Carta').style.animation=''
-        
-    }, 5000);
-    console.log(0/0)
+    return new Promise((resolve,reject)=>{
+        console.log("entrou")
+        document.getElementById('Carta').style.animation='5000ms abrir'
+        let card= randomCard()
+        setTimeout(function() {
+            document.getElementById('Carta').style.backgroundImage='url("./assets/WhatsApp Image 2026-03-02 at 23.57.28.jpeg")'
+            document.getElementById('Carta').style.height="90%"
+            document.getElementById('Carta').style.width="90%"
+            document.getElementById('Carta').style.backgroundPosition="top right"
+            document.getElementById('Carta').innerHTML=``
+            document.getElementById('Carta').innerHTML=`<h1 id='tituloCarta'>${card.titulos}</h1>`
+            document.getElementById('Carta').innerHTML+=`${card.text}`
+        }, 2500);
+        setTimeout(function() {
+            document.getElementById('Carta').style.animation=''
+            resolve()
+            console.log("acabou")
+        }, 5000);
+    })
+}
+async function QueerControl(){
+    if(runing){
+        next = true
+        return
+    }
+
+    runing = true
+
+    await CardInit()
+
+    runing = false
+
+    if(next){
+        next = false
+        setTimeout(function() {
+            QueerControl()
+        },1000)
+    }
+    console.log(next,runing)
 }
